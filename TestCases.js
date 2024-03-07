e0f8ea7dc82b168cd2ee95e8536a3b26d8894063
Charlie Street <83096653+charlie-street@users.noreply.github.com>
2024-03-07 17:32:33 -0600

    Add testLocation to our test cases

--- TestCases.js
+++ TestCases.js
@@ -1,5 +1,57 @@
 //Storage of test cases
+COUNTRIES = [
+    "Afghanistan", "Albania", "Algeria", "Andorra", "Angola", "Antigua & Deps", "Argentina", "Armenia", "Australia", "Austria", "Azerbaijan", "Bahamas", "Bahrain", "Bangladesh", "Barbados", "Belarus", "Belgium", "Belize", "Benin", "Bhutan", "Bolivia", "Bosnia Herzegovina", "Botswana", "Brazil", "Brunei", "Bulgaria", "Burkina", "Burundi", "Cambodia", "Cameroon", "Canada", "Cape Verde", "Central African Rep", "Chad", "Chile", "China", "Colombia", "Comoros", "Congo", "Congo {Democratic Rep}", "Costa Rica", "Croatia", "Cuba", "Cyprus", "Czech Republic", "Denmark", "Djibouti", "Dominica", "Dominican Republic", "East Timor", "Ecuador", "Egypt", "El Salvador", "Equatorial Guinea", "Eritrea", "Estonia", "Ethiopia", "Fiji", "Finland", "France", "Gabon", "Gambia", "Georgia", "Germany", "Ghana", "Greece", "Grenada", "Guatemala", "Guinea", "Guinea-Bissau", "Guyana", "Haiti", "Honduras", "Hungary", "Iceland", "India", "Indonesia", "Iran", "Iraq", "Ireland {Republic}", "Israel", "Italy", "Ivory Coast", "Jamaica", "Japan", "Jordan", "Kazakhstan", "Kenya", "Kiribati", "Korea North", "Korea South", "Kosovo", "Kuwait", "Kyrgyzstan", "Laos", "Latvia", "Lebanon", "Lesotho", "Liberia", "Libya", "Liechtenstein", "Lithuania", "Luxembourg", "Macedonia", "Madagascar", "Malawi", "Malaysia", "Maldives", "Mali", "Malta", "Marshall Islands", "Mauritania", "Mauritius", "Mexico", "Micronesia", "Moldova", "Monaco", "Mongolia", "Montenegro", "Morocco", "Mozambique", "Myanmar, {Burma}", "Namibia", "Nauru", "Nepal", "Netherlands", "New Zealand", "Nicaragua", "Niger", "Nigeria", "Norway", "Oman", "Pakistan", "Palau", "Panama", "Papua New Guinea", "Paraguay", "Peru", "Philippines", "Poland", "Portugal", "Qatar", "Romania", "Russian Federation", "Rwanda", "St Kitts & Nevis", "St Lucia", "Saint Vincent & the Grenadines", "Samoa", "San Marino", "Sao Tome & Principe", "Saudi Arabia", "Senegal", "Serbia", "Seychelles", "Sierra Leone", "Singapore", "Slovakia", "Slovenia", "Solomon Islands", "Somalia", "South Africa", "South Sudan", "Spain", "Sri Lanka", "Sudan", "Suriname", "Swaziland", "Sweden", "Switzerland", "Syria", "Taiwan", "Tajikistan", "Tanzania", "Thailand", "Togo", "Tonga", "Trinidad & Tobago", "Tunisia", "Turkey", "Turkmenistan", "Tuvalu", "Uganda", "Ukraine", "United Arab Emirates", "United Kingdom", "United States", "Uruguay", "Uzbekistan", "Vanuatu", "Vatican City", "Venezuela", "Vietnam", "Yemen", "Zambia", "Zimbabwe"
+];
 
+class Location {
+    constructor(lat,lon,country){
+        this.latitude = lat 
+        this.longitude = lon
+        this.country = country
+    }
+}
+
+function getLocation(lat,lon,country){
+
+
+
+    newLocation = new Location(0,0,"none");
+
+    if(lat < -90 || lat > 90){
+       return 'Error: Invalid latitude value'
+    }
+
+    if(lon < -180 || lon > 180){
+        return 'Error: Invalid longitude value'
+    }
+    if(!COUNTRIES.includes(country)){
+        return 'Error: Country does not exist'
+    }
+
+    newLocation.country = country 
+    newLocation.lat = lat 
+    newLocation.lon = lon 
+
+    return newLocation
+}
+
+function testLocation(){
+    //pass
+
+    results = []
+
+    results.push(getLocation(80.234,110.05607,"Andorra"))
+    results.push(getLocation(-30,0.0001,"Austria"))
+    
+    //fail
+
+    results.push(getLocation(-80000, 100000, "your moms house lol"))
+    results.push(getLocation(0, 0, "US"))
+
+    console.log(results)
+}
+
+testLocation()
 //Unit: getUser(username)
 function getUser(username) 
 {
