450e026ebac3ed383ce90a41c0d4dd808dbde22e
Camil Gosmanov <92610400+camilgosmanov@users.noreply.github.com>
2024-04-18 17:35:23 -0500

    Ticket 5: Task 8 Add Wemoo files

---
+++ wemoo/server.js
@@ -0,0 +1,90 @@
+const path = require("path");
+const http = require("http");
+const express = require("express");
+const socketio = require("socket.io");
+const formatMessage = require("./utils/messages");
+const createAdapter = require("@socket.io/redis-adapter").createAdapter;
+const redis = require("redis");
+require("dotenv").config();
+const { createClient } = redis;
+const {
+  userJoin,
+  getCurrentUser,
+  userLeave,
+  getRoomUsers,
+} = require("./utils/users");
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+// Set static folder
+app.use(express.static(path.join(__dirname, "public")));
+
+const botName = "Wemoo Bot";
+
+(async () => {
+  // Use the connection string for your Redis instance here:
+  const REDIS_URL = process.env.REDIS_URL || "redis://:lQ0JDSAXdBnEqr8Ur0vVBQsIhpLpDkLqRAzCaKsnaXc=@wemoo.redis.cache.windows.net:6380";
+  pubClient = createClient({ url: REDIS_URL });
+  await pubClient.connect();
+  subClient = pubClient.duplicate();
+  io.adapter(createAdapter(pubClient, subClient));
+})();
+
+
+// Run when client connects
+io.on("connection", (socket) => {
+  console.log(io.of("/").adapter);
+  socket.on("joinRoom", ({ username, room }) => {
+    const user = userJoin(socket.id, username, room);
+
+    socket.join(user.room);
+
+    // Welcome current user
+    socket.emit("message", formatMessage(botName, "Welcome to Wemoo!"));
+
+    // Broadcast when a user connects
+    socket.broadcast
+      .to(user.room)
+      .emit(
+        "message",
+        formatMessage(botName, `${user.username} has joined the chat`)
+      );
+
+    // Send users and room info
+    io.to(user.room).emit("roomUsers", {
+      room: user.room,
+      users: getRoomUsers(user.room),
+    });
+  });
+
+  // Listen for chatMessage
+  socket.on("chatMessage", (msg) => {
+    const user = getCurrentUser(socket.id);
+
+    io.to(user.room).emit("message", formatMessage(user.username, msg));
+  });
+
+  // Runs when client disconnects
+  socket.on("disconnect", () => {
+    const user = userLeave(socket.id);
+
+    if (user) {
+      io.to(user.room).emit(
+        "message",
+        formatMessage(botName, `${user.username} has left the chat`)
+      );
+
+      // Send users and room info
+      io.to(user.room).emit("roomUsers", {
+        room: user.room,
+        users: getRoomUsers(user.room),
+      });
+    }
+  });
+});
+
+const PORT = process.env.PORT || 8080;
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
