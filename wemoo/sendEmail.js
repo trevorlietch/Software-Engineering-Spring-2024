f92533692fa49954f2dec91adaebe013a3870bdc
trevorlietch <lietchtrevor@gmail.com>
2024-05-01 16:47:53 -0500

    coments for nodemailer

--- wemoo/sendEmail.js
+++ wemoo/sendEmail.js
@@ -4,14 +4,19 @@ let transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
     user: 'wemoo.service@gmail.com',
-    pass: 'qwca yphp nyst rprx' // Use the generated application-specific password here
+    //app passkey for wemoo.sercive email
+    pass: 'qwca yphp nyst rprx'
   }
 });
 
 let mailOptions = {
+  //this is the email which will send the link
   from: 'wemoo.service@gmail.com',
+  //this is changed to a variable to be sent 
   to: 'lietchtrevor@gmail.com',
+  //subject line
   subject: 'Join the chat',
+  //correct link to wemoo
   text: 'Click on the link to join the chat: http://wemoo.lol'
 };
 
